e9061fec80ffec830c9de52ea258021d26ffd99a
elessandroabreu <binho_abreu@hotmail.com>
2025-07-29 20:49:12 -0300

    enviando classe produto

---
+++ src/Produto.java
@@ -0,0 +1,11 @@
+class Produto {
+    String nome;
+    String categoria;
+    double preco;
+
+    public Produto(String nome, String categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+}
